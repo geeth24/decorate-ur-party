84a3eb6d50ea4ff436d41e39ceffa8dd4de41b18
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-12-17 01:38:59 -0600

    Remove quotes

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -155,7 +155,7 @@ function Pricing() {
                                 Contact for Pricing
                             </Text>
                             <Text size="sm" color="dimmed" mt="sm">
-                                "Lorem ipsum dolor sit amet, consectetur
+                                Lorem ipsum dolor sit amet, consectetur
                                 adipiscing elit, sed do eiusmod tempor
                                 incididunt ut labore et dolore magna aliqua. Ut
                                 enim ad minim veniam, quis nostrud exercitation
@@ -165,7 +165,7 @@ function Pricing() {
                                 dolore eu fugiat nulla pariatur. Excepteur sint
                                 occaecat cupidatat non proident, sunt in culpa
                                 qui officia deserunt mollit anim id est
-                                laborum."
+                                laborum.
                             </Text>
                             <Button
                                 mt="xl"
