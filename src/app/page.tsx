c87b6d845d65fa488b8cb8d1453eb7727bb9957d
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-05-08 14:46:48 -0500

    chore: Remove unused SVG files and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 function Page() {
   return (
-    <div>
-      Decorate Ur Party
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-100">
+      <Image
+        src="/Decorate Ur Party Final Logo 1x1.png"
+        alt="Decorate Ur Party Logo"
+        width={500}
+        height={500}
+      />
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
